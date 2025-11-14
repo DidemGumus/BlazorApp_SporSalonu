1c34caf6262dcaccaf8f81c8239cffcfe09f6099
DidemGumus <127394779+DidemGumus@users.noreply.github.com>
2025-11-14 16:14:06 +0300

    SporSalonu

---
+++ MyBlazorApp/Models/Membership.cs
@@ -0,0 +1,12 @@
+namespace MyBlazorApp.Models;
+
+public class Membership
+{
+    public string Id { get; set; } = "";
+    public string Name { get; set; } = "";
+    public decimal Price { get; set; }
+    public int DurationDays { get; set; }
+    public List<string> Features { get; set; } = new();
+}
+
+
